6e1ddcda91d5bdc6ac124a8aefd32648c6d8986e
Luis-Golac <luis.golac@utec.edu.pe>
2023-09-12 02:40:13 -0500

    pruebas

--- methods.h
+++ methods.h
@@ -1,11 +1,30 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
 struct Record1{
-  int key;
+  char key[8];
+  char nombre[20];
+  char producto[20];
+  char marca[20];
+  float precio;
+  int cantidad;
+
+  Record1(const char key_[8], char nombre_[20], char producto_[20],char marca_[20],float precio_, int cantidad_){
+    strncpy(key, key_, sizeof(key));
+    strncpy(nombre, nombre_, sizeof(nombre));
+    strncpy(producto, producto_, sizeof(producto));
+    strncpy(marca, marca_, sizeof(marca));
+    this->precio = precio_;
+    this->cantidad = cantidad_;
+  };
+
+  void print(){
+    cout << key << "\t" << nombre << "\t" << producto << "\t" << marca << "\t" << precio << "\t" << cantidad << endl;
+  }
 };
 
 template<typename T>
---
+++ prueba2.cpp
@@ -0,0 +1,80 @@
+// #include "BPlusFile.h"
+#include "methods.h"
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <cstring>
+void ingreso(){
+  ifstream archivo("datos.csv");
+  
+  if (!archivo.is_open()) {
+    cout << "Error al abrir el archivo" << endl;
+    return;
+  }
+
+  string linea;
+  vector<string> campos;
+
+  istringstream lineaStream(linea);
+
+  while (getline(archivo, linea)) {
+    istringstream lineaStream(linea);
+    string campo;
+
+    while (getline(lineaStream, campo, ',')) {
+      campos.push_back(campo);
+    }
+
+    char key[8];
+    char nombre[20];
+    char producto[20];
+    char marca[20];
+    float precio;
+    int cantidad;
+    
+    strncpy(key, campos[0].c_str(), sizeof(key) - 1);
+    key[sizeof(key) - 1 ]= '\0';
+  
+    
+    strncpy(nombre, campos[1].c_str(), sizeof(nombre) - 1);
+    nombre[sizeof(nombre) - 1 ]= '\0';
+
+
+    strncpy(producto, campos[2].c_str(), sizeof(producto) - 1);
+    producto[sizeof(producto) - 1 ]= '\0';
+
+    strncpy(marca, campos[3].c_str(), sizeof(marca) - 1);
+    marca[sizeof(marca) - 1 ]= '\0';
+
+    // cout << campos[4] << endl;
+    precio = stof(campos[4]);
+    cantidad = stoi(campos[5]);
+
+    Record1 record(key,nombre,producto,marca,precio,cantidad);
+    record.print();
+    string asd;
+    cin >> asd;
+
+    // for (const string& valor: campos){
+    //   cout << valor << "\t";
+    // }
+    // cout << endl;
+
+    campos.clear();
+  }
+
+  archivo.close();
+
+  return;
+}
+
+int main(){
+
+  ingreso();
+  
+
+
+  return 0;
+}
