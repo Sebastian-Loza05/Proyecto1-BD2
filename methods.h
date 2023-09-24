87ac83bfcd2ada279687c3c0a8c59c4fc2485f8e
Sebastian-Loza05 <sebastian.loza@utec.edu.pe>
2023-09-24 00:13:01 -0500

    remove final

--- methods.h
+++ methods.h
@@ -6,18 +6,18 @@
 using namespace std;
 
 struct Record1{
-  // long key;
-  char key[20];
+  long key;
+  // char key[20];
   char nombre[20];
   char producto[20];
   char marca[20];
   float precio;
   int cantidad;
 
-  Record1(char key_[20], char nombre_[20], char producto_[20],char marca_[20],float precio_, int cantidad_){
-  // Record1(long key_, char nombre_[20], char producto_[20],char marca_[20],float precio_, int cantidad_){
-    // this->key = key_;
-    strncpy(key, key_, sizeof(key));
+  // Record1(char key_[20], char nombre_[20], char producto_[20],char marca_[20],float precio_, int cantidad_){
+  Record1(long key_, char nombre_[20], char producto_[20],char marca_[20],float precio_, int cantidad_){
+    this->key = key_;
+    // strncpy(key, key_, sizeof(key));
     strncpy(nombre, nombre_, sizeof(nombre));
     strncpy(producto, producto_, sizeof(producto));
     strncpy(marca, marca_, sizeof(marca));
